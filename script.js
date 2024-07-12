2152bec73c634ecb2e1a0c53ea30ae42a2180b87
anton <avainfoconseils@gmail.com>
2024-07-12 08:44:52 +0200

    Fixed minor changes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function moveCursor(e) {
 	let value = e.target.attributes.getNamedItem("data-value").value;
 	document.getElementById("scrollbar").children[0].style.marginLeft = value;
 	[...(e.target.parentElement.children)].forEach(element => {
-		element.style.color = element.style.color == "darkcyan" ? "black" : "darkcyan";
+		element.style.color = element.style.color === "darkcyan" ? "black" : "darkcyan";
 		console.log(element.style.color)
 	});
 }
